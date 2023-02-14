4f90b8bbe212dc1119ddf8055d051f5a8a27aaf8
Yury Lobkov <racoon.enot@gmail.com>
2023-02-14 05:56:50 -0300

    image field for model toy

--- toyshop/showcase/models.py
+++ toyshop/showcase/models.py
@@ -1,6 +1,20 @@
 from django.db import models
+import os
+from uuid import uuid4
 
 # Create your models here.
+
+def path_and_rename(instance, filename):
+    upload_to = 'toys_images'
+    ext = filename.split('.')[-1]
+    # get filename
+    if instance.pk:
+        filename = '{}.{}'.format(instance.pk, ext)
+    else:
+        # set filename as random string
+        filename = '{}.{}'.format(uuid4().hex, ext)
+    # return the whole path to the file
+    return os.path.join(upload_to, filename)
 
 
 class Toy(models.Model):
@@ -13,6 +27,7 @@ class Toy(models.Model):
     quantity = models.PositiveIntegerField()
     category = models.ForeignKey("Categories", on_delete=models.CASCADE)
     slug = models.SlugField(max_length=50, unique=True)
+    image = models.ImageField(null=True, upload_to=path_and_rename, blank=True, default='default.jpg')
 
     def __str__(self):
         return self.name
@@ -22,8 +37,20 @@ class Toy(models.Model):
 class Materials(models.Model):
     material = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.material
+    
+
 class Sizes(models.Model):
     size = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.size
+    
+
 class Categories(models.Model):
     category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category
+    
