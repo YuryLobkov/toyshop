43294f278c57496e1ef71b23c97da90824e1fb4b
Yury Lobkov <racoon.enot@gmail.com>
2023-02-14 05:42:10 -0300

    admin interface

--- toyshop/showcase/admin.py
+++ toyshop/showcase/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+admin.site.register(Toy)
+admin.site.register(Materials)
+admin.site.register(Sizes)
+admin.site.register(Categories)
