2e7f78a03ba2a14319023492091647001b815d1c
dmitriy-kondrashkin <dimon8967@mail.ru>
2023-02-16 00:06:44 +0500

    toy detail view description fixed

--- toyshop/showcase/views.py
+++ toyshop/showcase/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 """
 PROJECT IMPORTS
 """
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
 from .models import Toy
 
 # Create your views here.
