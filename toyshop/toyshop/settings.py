35ba3b180c112822e7a8775ba888fc134bc9ce20
Yury Lobkov <racoon.enot@gmail.com>
2023-02-15 01:21:29 -0300

    navbar static svg

--- toyshop/toyshop/settings.py
+++ toyshop/toyshop/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static/assets",
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Media content storage
 
