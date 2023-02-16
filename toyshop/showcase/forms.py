2a70a0a891ed0fd726e58358db1265c3ade6ba63
Yury Lobkov <racoon.enot@gmail.com>
2023-02-16 01:57:56 -0300

    custom order page style

--- toyshop/showcase/forms.py
+++ toyshop/showcase/forms.py
@@ -40,3 +40,19 @@ class OrderForm(forms.ModelForm):
             'preferable_messenger',
             'comment',
         ]
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder':'example@mail.com'}),
+            'customer_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Jane Joe'}),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'+995999999999'}),
+            'preferable_messenger': forms.RadioSelect(attrs={'class': ''}),
+            'comment': forms.Textarea(attrs={'class': 'form-control', 'placeholder':'Please describe what kind of toy you want, and I will contact you to clarify details'})
+            }
+
+
+    def __init__(self, *args, **kwargs):
+        super(OrderForm, self).__init__(*args, **kwargs)
+        self.fields['email'].label = "Your email"
+        self.fields['customer_name'].label = "Your name"
+        self.fields['phone_number'].label = "Phone number"
+        self.fields['preferable_messenger'].label = "How can we comunicate"
+        self.fields['comment'].label = "Comments for your order"
