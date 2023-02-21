939be167446fc4f250db3e1577d1e667abbc4c89
Yury Lobkov <racoon.enot@gmail.com>
2023-02-20 21:42:22 -0300

    urls slug fix 2

--- toyshop/toyshop/urls.py
+++ toyshop/toyshop/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', include('showcase.urls')),
     path('admin/', admin.site.urls), 
+    path('', include('showcase.urls')),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
