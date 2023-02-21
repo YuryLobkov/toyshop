8067e0936eab7333a8c3ea250ab81db5f51d530b
Yury Lobkov <racoon.enot@gmail.com>
2023-02-20 21:36:30 -0300

    urls slug fix

--- toyshop/showcase/urls.py
+++ toyshop/showcase/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', ShowcaseMainView.as_view(), name='main_view'),
-    path('<slug>', views.toy_detail, name='toy-detail'),
     path('purchase/<slug>', views.purchase, name='purchase'),
     path('order/', views.OrderPage.as_view(), name='order'),
     path('thank-you/<int:pk>', views.purchase_thank_you, name='thank-you'),
     path('order-table/', views.orders_table, name='order-table'),
+    path('<slug>/', views.toy_detail, name='toy-detail'),
 ]
